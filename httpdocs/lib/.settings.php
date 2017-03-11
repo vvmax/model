23d76644d44fd8b76e7bbc792b9767309ba5c505
Admin <vvmax01@mail.ru>
2017-03-11 11:01:09 +0300

    Пример файла настроек

---
+++ httpdocs/lib/.settings.php
@@ -0,0 +1,17 @@
+﻿<?php
+/**
+ * класс настроек сайта
+ * настроить и переменовать в seettings.php
+ *
+ * @author Maxim Vorobyev
+ * @version 1.0
+ * @changed 2015.10.14
+ */
+class Settings {
+    const host='*****';
+    const basename='*****';
+    const password='*****';
+    const user='*****';
+    const sol='*****';
+	const MAIL='*****';
+}
